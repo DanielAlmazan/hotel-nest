284d6481efa74673a8f2d3fa10e571174455d9b1
Aitor Moreno <119342012+LtVish@users.noreply.github.com>
2024-05-14 20:03:42 +0200

    pruebita again

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ config();
 @Module({
   imports: [
     LimpiezaModule,
-    MongooseModule.forRoot(`mongodb://hotelDB:27017/hotel`),
+    MongooseModule.forRoot('mongodb://hotelDB/hotel'),
     UsuarioModule,
     AuthModule,
     UsuarioModule,
